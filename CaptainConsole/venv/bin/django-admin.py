60e9a9b07d4658cb67e84e0292830ca0b32ad314
Pálína Kröyer <palinakroyer@Palinas-MacBook-Pro-2.local>
2020-05-05 09:14:14 +0000

    captainconsole

---
+++ CaptainConsole/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/palinakroyer/Desktop/Skóli/6.önn/Verk2/project/Verklegt2/CaptainConsole/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
